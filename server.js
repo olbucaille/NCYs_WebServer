e2922c7fdf30e9f86727b38cd2660038d038d3b2
olbucaille <olivier.bucaille@isep.fr>
2016-03-30 00:20:27 +0200

    ajout iron ajax clock j'aurai pas du ajouter les bower components,... Signed-off-by: olbucaille <olivier.bucaille@isep.fr>

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function(req, res) {
-  res.end("Hello World");  
-});
-
-server.listen(1337);
-console.log("Serveur web lancé sur localhost:1337 ...");
